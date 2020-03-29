b850021c473a9bfcc3e53d63d719403040679cc6
joel123vilca <joevil23tjq@gmail.com>
2020-03-29 18:27:01 -0500

    webpack config

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import {showAlert} from './messages';
+import './styles.css';
+document.getElementById('btn-alert')
+.addEventListener('click',showAlert)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: '/Applications/joel/frontend-project/src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+      port: 5000
+  },
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin({filename: 'bundle.css'})
+  ]
+};
